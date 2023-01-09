825279dbb424c17486cfdadb12f2850a5d3a7c8b
Raymond Umukoro <reiymondjon134@gmail.com>
2023-01-09 13:03:59 +0100

    Creating product card

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,5 @@
+import data from './data'
+
 function App() {
   return (
     <div>
@@ -5,10 +7,28 @@ function App() {
         <a href="/">Horbax</a>
       </header>
       <main>
-        list products
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div key={product.slug} className="product">
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>${product.price}</strong>
+                </p>
+                <button> Add to Cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
       </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
