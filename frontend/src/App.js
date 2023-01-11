243a1e0cb020904efb57cbfc77fb3da51ebd606e
Raymond Umukoro <reiymondjon134@gmail.com>
2023-01-11 08:24:07 +0100

    Implementing react-bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,33 @@
-import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
-import { HomeScreen, ProductScreen } from './screens';
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom'
+import { HomeScreen, ProductScreen } from './screens'
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
+import { LinkContainer } from 'react-router-bootstrap'
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      <div className="d-flex flex-column site-container">
         <header>
-          <Link to="/">Horbax</Link>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>Horbax</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
         <main>
-          <Routes>
-            <Route path="/product/:slug" element={< ProductScreen />} />
-            <Route path="/" element={<HomeScreen />} />
-          </Routes>
+          <Container>
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+          </Container>
         </main>
+        <footer>
+          <div className="text-center">All rights reserved</div>
+        </footer>
       </div>
     </BrowserRouter>
   )
