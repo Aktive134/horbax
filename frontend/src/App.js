b137269bc25ef4807cfa257913ecf7089638fa5e
Raymond Umukoro <reiymondjon134@gmail.com>
2023-01-11 10:19:07 +0100

    Implemented product, rating component and refactored the homeScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes, Link } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { HomeScreen, ProductScreen } from './screens'
 import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container'
