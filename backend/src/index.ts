74882f2f6a33b5886d9950d27514fe157ff73dea
Raymond Umukoro <reiymondjon134@gmail.com>
2023-01-20 20:57:53 +0100

    Implemented the connection of mongoDB database

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,9 +7,9 @@ import mongoose from 'mongoose';
 dotenv.config();
 app.listen(Configuration.serverPort , () => console.log(`${Constant.messages.serverUp} ${Configuration.serverPort}`));
 
-// mongoose.connect(Configuration.Database.url).then(() => {
-//     console.log('connected to mongoDb');
-// }).catch((err) => {
-//     console.error('could not connect to mongoDb\n', err);
-// })
+mongoose.connect(Configuration.Database.url).then(() => {
+    console.log('connected to mongoDb');
+}).catch((err) => {
+    console.error('could not connect to mongoDb\n', err);
+})
 
