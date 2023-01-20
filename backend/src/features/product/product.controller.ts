af2c19e33fe1882f961061632fbb4ecf2b7bdf49
Raymond Umukoro <reiymondjon134@gmail.com>
2023-01-20 21:59:28 +0100

    Updated the product controller in features folder(backend)

--- backend/src/features/product/product.controller.ts
+++ backend/src/features/product/product.controller.ts
@@ -1,46 +1,58 @@
-import { Request, Response, NextFunction } from 'express';
-import ApplicationError from '../../common/error-handler/ApplicationError';
-import Constant from '../../constant';
-import data from '../../data';
+import { Request, Response, NextFunction } from 'express'
+import ApplicationError from '../../common/error-handler/ApplicationError'
+import Constant from '../../constant'
+import data from '../../data'
+import Product from './product.model'
 
-const Messages = Constant.messages;
+const Messages = Constant.messages
 
 class productController {
-    async getProductsHandler(req: Request, res: Response, next: NextFunction) {
-        try {
-            res.send(data.products);
-        } catch (error) {
-            console.log(error);
-        }
+  async getProductsHandler(req: Request, res: Response, next: NextFunction) {
+    try {
+      const products = await Product.find({})
+      res.status(200).send(products)
+    } catch (error: any) {
+      return next(new ApplicationError(error.message))
     }
+  }
 
-    async getProductHandler (req: Request, res: Response, next: NextFunction) {
-        try {
-            const { slug } = req.params;
-            const { products } = data;
-            const product = products.find((item) => item.slug === slug);
-            if(product) {
-                return res.send(product);
-            }
-            res.status(404).send({message: 'Product Not Found'});
-        } catch (error) {
-            console.log(error);
-        }
+  async getProductBySlugHandler(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) {
+    try {
+      const { slug } = req.params
+      const product = await Product.findOne({ slug })
+      if (product) {
+        return res.status(200).send(product)
+      }
+      res.status(404).send({ message: 'Product Not Found' })
+    } catch (error: any) {
+      return next(new ApplicationError(error.message))
     }
-    async getProductByIdHandler (req: Request, res: Response, next: NextFunction) {
-        try {
-            const { id } = req.params;
-            const { products } = data;
-            const product = products.find((item) => item._id === id);
-            if(product) {
-                return res.send(product);
-            }
-            res.status(404).send({message: 'Product Not Found'});
-        } catch (error) {
-            console.log(error);
-        }
+  }
+  async getProductByIdHandler(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { id } = req.params
+      const product = await Product.findById({ _id: id })
+      if (product) {
+        return res.send(product)
+      }
+      res.status(404).send({ message: 'Product Not Found' })
+    } catch (error: any) {
+      console.log(error)
     }
+  }
+  async createProductHandler(req: Request, res: Response, next: NextFunction) {
+    try {
+      const product = new Product(req.body)
+      await product.save()
+      res.status(201).send(product)
+    } catch (error: any) {
+      return next(new ApplicationError(error.message))
+    }
+  }
 }
 
-export default new productController();
-
+export default new productController()
--- backend/src/features/product/product.routes.ts
+++ backend/src/features/product/product.routes.ts
@@ -4,12 +4,13 @@ import productController from './product.controller';
 const productRouter = Router();
 const {
     getProductsHandler,
-    getProductHandler,
-    getProductByIdHandler
+    getProductBySlugHandler,
+    getProductByIdHandler,
+    createProductHandler
 } = productController;
 
-productRouter.route('/products').get(getProductsHandler);
-productRouter.route('/products/slug/:slug').get(getProductHandler);
+productRouter.route('/products').get(getProductsHandler).post(createProductHandler);
+productRouter.route('/products/slug/:slug').get(getProductBySlugHandler);
 productRouter.get('/products/:id', getProductByIdHandler);
 
 export default productRouter;
